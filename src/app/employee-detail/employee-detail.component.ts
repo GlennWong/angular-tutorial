510159e5776be112dadcf9e60d0db6ad87b0324f
GlennWong <doc.wgl@163.com>
2019-02-11 19:04:08 +0800

    stash http error handler

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../employee.service';
+import { Component, OnInit } from "@angular/core";
+import { EmployeeService } from "../employee.service";
 
 @Component({
-  selector: 'app-employee-detail',
+  selector: "app-employee-detail",
   template: `
     <h3>Employee Detail</h3>
+    {{ errorMsg }}
     <ul *ngFor="let employee of employees">
       <li>{{ employee.id }} {{ employee.name }} {{ employee.age }}</li>
     </ul>
   `,
-  styleUrls: ['./employee-detail.component.scss']
+  styleUrls: ["./employee-detail.component.scss"]
 })
 export class EmployeeDetailComponent implements OnInit {
-
   public employees = [];
+  public errorMsg;
 
   constructor(private _employeeService: EmployeeService) {}
 
   ngOnInit() {
-    this._employeeService.getEmployees()
-      .subscribe(data => this.employees = data);
+    this._employeeService
+      .getEmployees()
+      .subscribe(
+        data => (this.employees = data),
+        error => (this.errorMsg = error)
+      );
   }
-
 }
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../employee.service';
+import { Component, OnInit } from "@angular/core";
+import { EmployeeService } from "../employee.service";
 @Component({
-  selector: 'app-employee-list',
+  selector: "app-employee-list",
   template: `
     <h3>Employee List</h3>
+    {{ errorMsg }}
     <ul *ngFor="let employee of employees">
-      <li> {{ employee.name }}</li>
+      <li>{{ employee.name }}</li>
     </ul>
   `,
-  styleUrls: ['./employee-list.component.scss']
+  styleUrls: ["./employee-list.component.scss"]
 })
 export class EmployeeListComponent implements OnInit {
-
   public employees = [];
+  public errorMsg;
 
   constructor(private _employeeService: EmployeeService) {}
 
   ngOnInit() {
-    this._employeeService.getEmployees()
-      .subscribe(data => this.employees = data);
+    this._employeeService
+      .getEmployees()
+      .subscribe(
+        data => (this.employees = data),
+        error => (this.errorMsg = error)
+      );
   }
-
 }
