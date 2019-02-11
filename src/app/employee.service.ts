07117a4f6967c2174412c4423c891159a011cfcb
GlennWong <doc.wgl@163.com>
2019-02-11 18:42:58 +0800

    http observable

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IEmployee } from './employee';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() {}
+  private _url: string = "/assets/data/employees.json";
 
-  getEmployees() {
-    return [
-      {"id": 1, "name": "Glen", "age": 31},
-      {"id": 2, "name": "Glen2", "age": 32},
-      {"id": 3, "name": "Glen3", "age": 33}
-    ];
+  constructor(private http: HttpClient) {}
+
+  getEmployees(): Observable<IEmployee[]> {
+    return this.http.get<IEmployee[]>(this._url);
+    
   }
 }
