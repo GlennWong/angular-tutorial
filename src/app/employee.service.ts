8eee9699d36092454dff3719d7e6a17bbc554345
GlennWong <doc.wgl@163.com>
2019-02-12 09:58:39 +0800

    update angular 6 with catch Error

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { IEmployee } from "./employee";
-import { Observable } from "rxjs";
-import "rxjs/add/operator/catch";
-import "rxjs/add/observable/throw";
+
+import { Observable, EMPTY, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -14,10 +14,14 @@ export class EmployeeService {
   constructor(private http: HttpClient) {}
 
   getEmployees(): Observable<IEmployee[]> {
-    return this.http.get<IEmployee[]>(this._url).catch(this.errorHandler);
-  }
-
-  errorHandler(error: HttpErrorResponse) {
-    return Observable.throw(error.message || "Server Error!");
+    return this.http.get<IEmployee[]>(this._url).pipe(
+      catchError(err => {
+        if (err.status == 401) {
+          return EMPTY;
+        } else {
+          return throwError(err.message);
+        }
+      })
+    );
   }
 }
